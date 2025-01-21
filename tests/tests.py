a4ffb8a27df9a10572bf587d3908b1fa30b9a2f3
Max Schwarz <max.schwarz@online.de>
2025-01-21 16:10:11 +0100

    unit tests for python API

--- tests/tests.py
+++ tests/tests.py
@@ -1,67 +1,20 @@
 import unittest
-import subprocess
-import json
-import tempfile
-from pathlib import Path
+import clang_highlight
+from typing import Tuple, Optional
+from clang_highlight import TokenType, Token
 
 
 class CHTests(unittest.TestCase):
+    def get_token(
+        self, highlighted: clang_highlight.HighlightedCode, fragment: str
+    ) -> Tuple[str, Optional[Token]]:
+        offset = highlighted.code.index(fragment.encode("utf8"))
 
-    def run_ch(self, code: bytes):
-        with tempfile.TemporaryDirectory(delete=False) as build_dir:
-            build_dir = Path(build_dir)
-            code_path = build_dir / 'code.cpp'
-            with open(code_path, 'wb') as code_file:
-                code_file.write(code)
+        for text, token in highlighted:
+            if token and token.offset == offset:
+                return text, token
 
-            with open(build_dir / 'compile_commands.json', 'w') as db_file:
-                db_file.write(
-                    json.dumps([{
-                        'directory': str(build_dir),
-                        'command':
-                        f'/usr/bin/c++ -DNDEBUG -std=c++20 -Wall {code_path}',
-                        'file': str(code_path),
-                    }]))
-
-            result = subprocess.run(
-                ['clang-highlight', '-p', build_dir, '--json-out', code_path],
-                stdout=subprocess.PIPE)
-
-        self.assertEqual(result.returncode, 0)
-
-        data = json.loads(result.stdout.decode('utf8'))
-
-        self.assertEqual(data['file'], str(code_path))
-        self.validate_tokens(data['tokens'], code)
-
-        return data['tokens']
-
-    def validate_tokens(self, tokens: list, code: bytes):
-        code_offset = 0
-
-        for token in tokens:
-            self.assertIn('type', token)
-
-            off = token['offset']
-            length = token['length']
-            self.assertGreaterEqual(off, code_offset)
-            self.assertGreater(length, 0)
-
-            # All intermediate is whitespace
-            if off > code_offset:
-                self.assertTrue(
-                    code[code_offset:off].isspace(),
-                    f"{repr(code[code_offset:off])} at offset {code_offset} is not whitespace but has no token. Entire code:\n{code.decode()}"
-                )
-
-            code_offset = off + length
-
-    def get_token(self, code: bytes, tokens: list, fragment: bytes):
-        offset = code.index(fragment)
-        matching = [tok for tok in tokens if tok['offset'] == offset]
-        self.assertEqual(len(matching), 1)
-
-        return matching[0]
+        return None, None
 
     def test_simple(self):
         code = """
@@ -71,17 +24,18 @@ class CHTests(unittest.TestCase):
             std::cout << "Hello World!\\n";
             return 0;
         }
-        """.encode('utf8')
+        """
 
-        tokens = self.run_ch(code)
+        h = clang_highlight.run(code=code)
+        self.assertEqual(len(h.diagnostics), 0)
 
-        tok_include = self.get_token(code, tokens, b'#include')
-        self.assertEqual(tok_include['type'], "preprocessor")
+        _, tok_include = self.get_token(h, "#include")
+        self.assertEqual(tok_include.type, TokenType.PREPROCESSOR)
 
-        tok_cout = self.get_token(code, tokens, b'cout')
-        self.assertEqual(tok_cout['type'], 'variable')
-        self.assertIn('link', tok_cout)
-        self.assertEqual(tok_cout['link']['qualified_name'], 'std::cout')
+        _, tok_cout = self.get_token(h, "cout")
+        self.assertEqual(tok_cout.type, TokenType.VARIABLE)
+        self.assertTrue(tok_cout.link)
+        self.assertEqual(tok_cout.link.qualified_name, "std::cout")
 
     def test_template_inst(self):
         code = """
@@ -96,14 +50,14 @@ class CHTests(unittest.TestCase):
             std::vector<int> v;
             myLambda(v);
         }
-        """.encode('utf8')
+        """
 
-        tokens = self.run_ch(code)
+        h = clang_highlight.run(code=code)
+        self.assertEqual(len(h.diagnostics), 0)
 
-        tok_help = self.get_token(code, tokens, b'emplace_back();')
-        self.assertEqual(tok_help['type'], 'name')
-        self.assertEqual(tok_help['link']['qualified_name'],
-                         'std::vector::emplace_back')
+        _, tok_help = self.get_token(h, "emplace_back();")
+        self.assertEqual(tok_help.type, TokenType.NAME)
+        self.assertEqual(tok_help.link.qualified_name, "std::vector::emplace_back")
 
     def test_unspecialize(self):
         code = """
@@ -145,33 +99,34 @@ class CHTests(unittest.TestCase):
 
             func(0);
         }
-        """.encode()
+        """
 
-        tokens = self.run_ch(code)
+        h = clang_highlight.run(code=code)
+        self.assertEqual(len(h.diagnostics), 0)
 
-        tok_test = self.get_token(code, tokens, b'test(0, true);')
-        self.assertEqual(tok_test['link']['qualified_name'], "Test::test")
-        self.assertEqual(tok_test['link']['parameter_types'], ['int', 'bool'])
+        _, tok_test = self.get_token(h, "test(0, true);")
+        self.assertEqual(tok_test.link.qualified_name, "Test::test")
+        self.assertEqual(tok_test.link.parameter_types, ["int", "bool"])
 
-        tok_test = self.get_token(code, tokens, b'func_template<bool>(0, true);')
-        self.assertEqual(tok_test['link']['qualified_name'], "Test::func_template")
-        self.assertEqual(tok_test['link']['parameter_types'], ['int', 'bool'])
-        self.assertEqual(tok_test['link']['line'], 10)
+        _, tok_test = self.get_token(h, "func_template<bool>(0, true);")
+        self.assertEqual(tok_test.link.qualified_name, "Test::func_template")
+        self.assertEqual(tok_test.link.parameter_types, ["int", "bool"])
+        self.assertEqual(tok_test.link.line, 10)
 
-        tok_test = self.get_token(code, tokens, b'func_template<int>(0, true);')
-        self.assertEqual(tok_test['link']['qualified_name'], "Test::func_template")
-        self.assertEqual(tok_test['link']['parameter_types'], ['int', 'bool'])
-        self.assertEqual(tok_test['link']['line'], 14)
+        _, tok_test = self.get_token(h, "func_template<int>(0, true);")
+        self.assertEqual(tok_test.link.qualified_name, "Test::func_template")
+        self.assertEqual(tok_test.link.parameter_types, ["int", "bool"])
+        self.assertEqual(tok_test.link.line, 14)
 
-        tok_type = self.get_token(code, tokens, b'Type<bool>')
-        self.assertEqual(tok_type['link']['qualified_name'], "Test::Type")
+        _, tok_type = self.get_token(h, "Type<bool>")
+        self.assertEqual(tok_type.link.qualified_name, "Test::Type")
 
-        tok_type = self.get_token(code, tokens, b'Type2 other2')
-        self.assertEqual(tok_type['link']['qualified_name'], "Type2")
+        _, tok_type = self.get_token(h, "Type2 other2")
+        self.assertEqual(tok_type.link.qualified_name, "Type2")
 
-        tok_call = self.get_token(code, tokens, b'func(0);')
-        self.assertEqual(tok_call['link']['qualified_name'], "func")
-        self.assertEqual(tok_call['link']['parameter_types'], ['T'])
+        _, tok_call = self.get_token(h, "func(0);")
+        self.assertEqual(tok_call.link.qualified_name, "func")
+        self.assertEqual(tok_call.link.parameter_types, ["T"])
 
 
 if __name__ == "__main__":
